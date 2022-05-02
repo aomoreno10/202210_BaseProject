a33b0ea413ca878257950e03ab7e809c688c7f15
Abel <abel@bl3ndlabs.com>
2022-05-01 22:00:46 -0500

    Parcial Abel Moreno - Initial component test

--- src/app/plantas/plantas.component.spec.ts
+++ src/app/plantas/plantas.component.spec.ts
@@ -1,14 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
+import { HttpClientModule } from '@angular/common/http';
+import { Planta } from './planta';
 import { PlantasComponent } from './plantas.component';
+import { PlantaService } from './planta.service';
 
 describe('PlantasComponent', () => {
   let component: PlantasComponent;
   let fixture: ComponentFixture<PlantasComponent>;
+  let debug: DebugElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PlantasComponent ]
+      imports: [HttpClientModule],
+      declarations: [ PlantasComponent ],
+      providers: [PlantaService],
     })
     .compileComponents();
   });
@@ -17,6 +25,36 @@ describe('PlantasComponent', () => {
     fixture = TestBed.createComponent(PlantasComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    component.plantas = [
+      new Planta(
+        1,
+        'nombre_comun 1',
+        'nombre_cientifico 1',
+        'tipo 1', 
+        120,
+        'clima 1',
+        'sustrato_siembra 1'
+      ),
+      new Planta(
+        2,
+        'nombre_comun 2',
+        'nombre_cientifico 2',
+        'tipo 2', 
+        120,
+        'clima 2',
+        'sustrato_siembra 2'
+      ),
+      new Planta(
+        3,
+        'nombre_comun 3',
+        'nombre_cientifico 3',
+        'tipo 3', 
+        120,
+        'clima 3',
+        'sustrato_siembra 3'
+      ),
+    ];
   });
 
   it('should create', () => {
